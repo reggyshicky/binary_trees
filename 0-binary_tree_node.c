6ca345d1c3f0c670c218e11722dd3b5ec526a7fb
reggyshicky <reginahshikanda@gmail.com>
2023-08-02 02:38:33 +0300

    creation of a newnode of a binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,9 +10,10 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
+
 	newnode = malloc(sizeof(binary_tree_t));
 	if (!newnode)
-		return NULL;
+		return (NULL);
 	newnode->n = value;
 	newnode->parent = parent;
 	newnode->left = NULL;
