1e973c640e41c7793237c765219b2f55b827d0a4
reggyshicky <reginahshikanda@gmail.com>
2023-08-03 12:37:52 +0300

    find the unce of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - gets the uncles of a node
+ * @node: pointer to the node to find the uncle
+ * Return: uncle of a node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}
