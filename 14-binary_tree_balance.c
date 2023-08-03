9715b631739adb52053dc97e776f2cc684cfaa33
reggyshicky <reginahshikanda@gmail.com>
2023-08-03 10:07:59 +0300

    getting the balance factor of a tree, the difference btn right height and left height

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure BF
+ * Return: balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+	int BF = 0;
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	BF = left_height - right_height;
+
+	return (BF);
+}
+
+/**
+ * binary_tree_height - measures height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_height;
+	size_t left_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
